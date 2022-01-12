053eb86b20d1a45d0fcb3adf5c059c73244016af
Kip Yin <28321392+kipyin@users.noreply.github.com>
2022-01-12 23:00:29 +0800

    feat: add basic board operations

--- src/hopscotch/hopscotch.py
+++ src/hopscotch/hopscotch.py
@@ -1,1 +1,77 @@
 """Main module."""
+from typing import List, TypeVar
+
+from attrs import define, field
+
+B = TypeVar("B", bound="Board")
+
+
+@define
+class Point:
+    """A point on a Hopscotch board."""
+
+    x: int
+    y: int
+    taken: bool = True
+
+
+@define
+class Board:
+    """A Hopscotch board."""
+
+    size: int
+    filled: bool = True
+    _board: List[Point] = field(init=False)
+
+    def __attrs_post_init__(self: B) -> None:
+        """Generate the board after declaring the size of the board."""
+        self._board = self._generate_board()
+
+    def _generate_board(self: B) -> List[Point]:
+        """Generate the board with all points filled."""
+        board = []
+        for x in range(self.size):
+            for y in range(self.size):
+                if x + y <= self.size - 1:
+                    board.append(Point(x, y, self.filled))
+                else:
+                    continue
+        return board
+
+    def reset(self: B) -> None:
+        """Reset the current board."""
+        self._board = self._generate_board()
+
+    def available_moves_of(self: B, x: int, y: int) -> List[Point]:
+        """List all available moves from a point."""
+        if Point(x, y, True) not in self._board:
+            raise ValueError(f"Point({x}, {y}) is empty!")
+        available = []
+        move_steps = [-2, 0, 2]
+        for x_delta in move_steps:
+            for y_delta in move_steps:
+                if (
+                    abs(x_delta) + abs(y_delta) != 4
+                    and self.has(x + x_delta, y + y_delta, False)  # noqa: W503
+                    and self.has(x + x_delta // 2, y_delta // 2, True)  # noqa: W503
+                ):
+                    available.append(self.point(x + x_delta, y + y_delta))
+        return available
+
+    def point(self: B, x: int, y: int) -> Point:
+        """Pinpoint a point on the board."""
+        for taken in [True, False]:
+            if self.has(x, y, taken):
+                return self._board[self._board.index(Point(x, y, taken))]
+        else:
+            raise ValueError(f"Point({x}, {y}) is out of the range.")
+
+    def has(self: B, x: int, y: int, taken: bool) -> bool:
+        """Check if a point is on the board."""
+        return Point(x, y, taken) in self._board
+
+    def set_point(self: B, x: int, y: int, taken: bool) -> bool:
+        """Set a point to taken or not taken."""
+        p = self.point(x, y)
+        self._board[self._board.index(p)] = Point(x, y, taken)  # TODO: refactor
+        return True
--- tests/test_hopscotch.py
+++ tests/test_hopscotch.py
@@ -4,6 +4,7 @@ from typing import Generator
 import pytest
 
 import hopscotch
+from hopscotch.hopscotch import Board, Point
 
 
 @pytest.fixture
@@ -15,3 +16,55 @@ def version() -> Generator[str, None, None]:
 def test_version(version: str) -> None:
     """Sample pytest test function with the pytest fixture as an argument."""
     assert version == "2022.0.0"
+
+
+def test_board_generation() -> None:
+    """A board contains 15 points with size of 5."""
+    board = Board(size=5)
+    assert 15 == len(board._board)
+
+
+def test_available_moves_of_empty_point() -> None:
+    """An empty point does not have any available moves."""
+    b = Board(size=5)
+    with pytest.raises(ValueError):
+        b._board[0] = Point(0, 0, taken=False)
+        b.available_moves_of(0, 0)
+
+
+def test_available_moves_of_origin() -> None:
+    """The max number available moves of the origin has is 2."""
+    b = Board(size=5, filled=False)
+    for x in [0, 1]:
+        for y in [0, 1]:
+            b.set_point(x, y, True)
+    assert [Point(0, 2, False), Point(2, 0, False)] == b.available_moves_of(0, 0)
+
+
+def test_board_point() -> None:
+    """Any point on the board can be fetched via `Board.point()` method."""
+    b = Board(5)
+    assert True is b.point(0, 0).taken
+
+
+def test_board_point_out_of_range() -> None:
+    """Raise `ValueError` if a point is outside of the board."""
+    b = Board(5)
+    with pytest.raises(ValueError):
+        b.point(5, 0)
+
+
+def test_board_has_piece() -> None:
+    """The board can check if a coordinate is a valid point on the board."""
+    b = Board(5)
+    assert b.has(0, 0, True)
+    assert not b.has(5, 5, True)
+    assert not b.has(1, 1, False)
+
+
+def test_set_point() -> None:
+    """The status of a point can be set via `set_point` method."""
+    b = Board(5)
+    assert True is b.point(0, 0).taken
+    b.set_point(0, 0, False)
+    assert False is b.point(0, 0).taken
